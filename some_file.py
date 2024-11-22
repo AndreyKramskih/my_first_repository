6383db0a7d804921562882365f12d0955cdc4a46
Andrey Kramskih <andreykramskih@gmail.com>
2024-11-22 16:14:23 +0300

    Автоматизируем запросы к Telegram Bot API

--- some_file.py
+++ some_file.py
@@ -1,3 +1,25 @@
-print('This is file from GitHub repository')
+import time
 
-print ("There are new local changes")
+import requests
+
+API_URL='https://api.telegram.org/bot'
+BOT_TOKEN='7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
+TEXT='Ура, классный апдейт!'
+MAXCOUNTER=100
+
+offset=-2
+counter=0
+chat_id:int
+
+while counter < MAXCOUNTER:
+    print('attempt =', counter) #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+
+    if updates['result']:
+        for result  in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+    time.sleep(1)
+    counter+=1
