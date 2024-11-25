2f63a0447c5fc4aeb7137c710a8c47199afbee95
Andrey Kramskih <andreykramskih@gmail.com>
2024-11-25 15:24:59 +0300

    Long polling

--- some_file.py
+++ some_file.py
@@ -4,25 +4,24 @@ import requests
 
 
 API_URL='https://api.telegram.org/bot'
-API_CATS_URL='https://api.thecatapi.com/v1/images/search'
-BOT_TOKEN='7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
+BOT_TOKEN= '7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
+offset = -2
+timeout = 60
+updates: dict
 
 
-
-offset=-2
-updates=dict
-
-def do_something() ->None:
+def do_something() -> None:
     print('Был апдейт')
 
+
 while True:
-    start_time=time.time()
-    updates=requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
-            offset=result['update_id']
+            offset = result['update_id']
             do_something()
-    time.sleep(3)
-    end_time=time.time()
-    print(f'Время между запросами к Telegram BOT API: {end_time-start_time}')
+
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
