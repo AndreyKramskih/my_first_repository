798f4cfb28dc20543aa34272dd86ca4b77ace9a5
Andrey Kramskih <andreykramskih@gmail.com>
2024-11-26 10:54:14 +0300

    Эхо бот

--- some_file.py
+++ some_file.py
@@ -1,27 +1,39 @@
-import time
 
-import requests
+#Из библиотеки aiogram импортируем классы Bot и Dispatcher
+from aiogram import Bot, Dispatcher
 
+#Из aiogram.filters импортируем класс Command,
+# чтобы фильтровать апдейты по наличию в них команд,
+# то есть сочетаний символов, начинающихся со знака "/".
+from aiogram.filters import Command
 
-API_URL='https://api.telegram.org/bot'
-BOT_TOKEN= '7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
-offset = -2
-timeout = 60
-updates: dict
+#Из  aiogram.types импортируем класс Message. Апдейты этого типа мы будем ловить эхо-ботом.
+from aiogram.types import Message
 
+BOT_TOKEN='7656570135:AAHAU-5sKUFNc5UfgpBqEd4fxrKQWcQpoeU'
 
-def do_something() -> None:
-    print('Был апдейт')
+# Создаем объекты бота и диспетчера
+# Инициализируем объекты бота и диспетчера. В качестве аргумента в Bot передаем BOT_TOKEN.
+bot=Bot(token=BOT_TOKEN)
+dp=Dispatcher()
 
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот\nНапиши мне что-нибудь')
 
-while True:
-    start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            do_something()
-
-    end_time = time.time()
-    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
+if __name__ =='__main__':
+    dp.run_polling(bot)
